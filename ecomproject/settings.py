d78a68dd8ed16f47498ae9facc4a86ad3d6c1258
chuchu387 <heyitsmeshubhamofficial69@gmail.com>
2024-03-07 22:34:03 +0545

    hosts allowed

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3&v@@h#_62=00n1@qc-j$ks+yq2jowu^+q)vnqhm&w&uc2y()-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
