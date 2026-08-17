8b248569cafb04d040245eff06a7d4c2b676c50e
Selroti0 <84653546+chuchu387@users.noreply.github.com>
2026-08-17 12:15:50 +0545

    fix: remove undeclared six dependency from password reset tokens

--- ecomapp/utils.py
+++ ecomapp/utils.py
@@ -1,13 +1,11 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-import six
 
 
 class MyPasswordResetTokenGenerator(PasswordResetTokenGenerator):
     """Token generator used for password reset links."""
+
     def _make_hash_value(self, user, timestamp):
-        return (
-            six.text_type(user.pk) + six.text_type(timestamp)
-        )
+        return str(user.pk) + str(timestamp)
 
 
 password_reset_token = MyPasswordResetTokenGenerator()
